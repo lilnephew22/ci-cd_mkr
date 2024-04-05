32d3637da1fe77eb2072966d685f7b26c9f5afe1
Andrew Marchuk <andrewgulban22@gmail.com>
2024-04-05 11:00:54 +0300

    def 2

--- task.py
+++ task.py
@@ -2,3 +2,6 @@ def read_file(filename):
     with open(filename, 'r') as file:
         return set(file.readlines())
 
+def write_to_file(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
